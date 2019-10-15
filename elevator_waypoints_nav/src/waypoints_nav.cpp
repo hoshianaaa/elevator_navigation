7fa94dfba9fb1c4dd1a016bf2e8bfc7aa54a17b7
hoshina <hoshina12345@gmail.com>
2019-10-15 20:53:57 +0900

    edit yamlRead function in waypoints_nav

--- elevator_waypoints_nav/src/waypoints_nav.cpp
+++ elevator_waypoints_nav/src/waypoints_nav.cpp
@@ -1,5 +1,13 @@
 #include <elevator_waypoints_nav/waypoints_nav.h>
 
+#ifdef NEW_YAMLCPP
+templete<typename T>
+void operator >> (const YAML::Node& node, T& i)
+{
+	i = node.as<T>();
+};
+#endif
+
 WaypointsNavigation::WaypointsNavigation() :
 	move_base_action_("move_base", true),
 	rate_(10)
@@ -39,6 +47,79 @@ void WaypointsNavigation::run(){
 		sleep();
 	}
 }
+
+bool readFile(const std::string &filename){
+	waypoints_.poses.clear();
+	try{
+			std::ifstream ifs(filename.c_str(), std::ifstream::in);
+			if(ifs.good() == false){
+					return false;
+			}
+
+			YAML::Node node;
+			
+			#ifdef NEW_YAMLCPP
+					node = YAML::Load(ifs);
+			#else
+					YAML::Parser parser(ifs);
+					parser.GetNextDocument(node);
+			#endif
+
+			#ifdef NEW_YAMLCPP
+					const YAML::Node &wp_node_tmp = node["waypoints"];
+					const YAML::Node *wp_node = wp_node_tmp ? &wp_node_tmp : NULL;
+			#else
+					const YAML::Node *wp_node = node.FindValue("waypoints");
+			#endif
+
+			geometry_msgs::Pose pose;
+			if(wp_node != NULL){
+					for(int i=0; i < wp_node->size(); i++){
+
+							(*wp_node)[i]["point"]["x"] >> pose.position.x;
+							(*wp_node)[i]["point"]["y"] >> pose.position.y;
+							(*wp_node)[i]["point"]["z"] >> pose.position.z;
+
+							waypoints_.poses.push_back(pose);
+
+					}
+			}else{
+					return false;
+			}
+			
+			#ifdef NEW_YAMLCPP
+					const YAML::Node &fp_node_tmp = node["finish_pose"];
+					const YAML::Node *fp_node = fp_node_tmp ? &fp_node_tmp : NULL;
+			#else
+					const YAML::Node *fp_node = node.FindValue("finish_pose");
+			#endif
+
+			if(fp_node != NULL){
+					(*fp_node)["pose"]["position"]["x"] >> pose.position.x;
+					(*fp_node)["pose"]["position"]["y"] >> pose.position.y;
+					(*fp_node)["pose"]["position"]["z"] >> pose.position.z;
+
+					(*fp_node)["pose"]["orientation"]["x"] >> pose.orientation.x;
+					(*fp_node)["pose"]["orientation"]["y"] >> pose.orientation.y;
+					(*fp_node)["pose"]["orientation"]["z"] >> pose.orientation.z;
+					(*fp_node)["pose"]["orientation"]["w"] >> pose.orientation.w;
+
+					waypoints_.poses.push_back(pose);
+
+			}else{
+					return false;
+			}
+
+	}catch(YAML::ParserException &e){
+			return false;
+
+	}catch(YAML::RepresentationException &e){
+			return false;
+	}
+
+	return true;
+}
+
 
 int main(int argc, char *argv[]){
 	ros::init(argc, argv, ROS_PACKAGE_NAME);
