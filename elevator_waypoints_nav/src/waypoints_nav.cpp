f33126d9f9df64d90686a103dcdd63d08a2bf724
hoshina <hoshina12345@gmail.com>
2019-10-15 23:07:06 +0900

    waypoints_nav making

--- elevator_waypoints_nav/src/waypoints_nav.cpp
+++ elevator_waypoints_nav/src/waypoints_nav.cpp
@@ -1,24 +1,42 @@
 #include <elevator_waypoints_nav/waypoints_nav.h>
 
-#ifdef NEW_YAMLCPP
-templete<typename T>
-void operator >> (const YAML::Node& node, T& i)
-{
-	i = node.as<T>();
-};
-#endif
-
 WaypointsNavigation::WaypointsNavigation() :
 	move_base_action_("move_base", true),
 	rate_(10)
 {
+
+	std::cout << "test" << std::endl;
 	while((move_base_action_.waitForServer(ros::Duration(1.0)) == false) && (ros::ok() == true))
 	{
 		ROS_INFO("waiting...");
 	}
 
+	ros::NodeHandle private_nh("~");
+	private_nh.param("filename", filename, filename)
+	ROS_INFO_STREAM("Read waypoints data from " << filename);
+	if(!readFile(filename)) {
+			ROS_ERROR("Failed loading waypoints file");
+	} else {
+			last_waypoint_ = waypoints_.poses.end()-2;
+			finish_pose_ = waypoints_.poses.end()-1;
+			computeWpOrientation();
+	}
+		current_waypoint_ = waypoints_.poses.begin();
+	} else {
+			ROS_ERROR("waypoints file doesn't have name");
+	}
 	world_frame_ = "map";
+
+	elevator_front_pose_.position.x = 3;
+	elevator_front_pose_.position.y = 0;
+	elevator_front_pose_.orientation = tf::createQuaternionMsgFromYaw(M_PI);
+
+	elevator_point_.x = 0;
+	elevator_point_.y = 0;
+
+
 }
+
 
 void WaypointsNavigation::startNavigationGL(const geometry_msgs::Point &dest){
 	geometry_msgs::Pose pose;
@@ -37,6 +55,23 @@ void WaypointsNavigation::startNavigationGL(const geometry_msgs::Pose &dest){
 	std::cout << "send goal" << std::endl;
 }
 
+void computeWpOrientation(){
+	double goal_direction;
+	for (std::vector<Waypoint>::iterator it = waypoints_.begin(); it != finish_pose_; it++) {
+		if ((it)->floor != (it+1)->floor){
+			goal_direction = atan2(elevator_front_pose_.position.y - (it)->geometry_msg.position.y,
+																		elevator_front_pose_.position.x - (it)->position.x);
+		}
+		else
+		{
+			goal_direction = atan2((it+1)->geometry_msg.position.y - (it)->geometry_msg.position.y,
+														 (it+1)->geometry_msg.position.x - (it)->geometry_msg.position.x);
+		}
+		(it)->orientation = tf::createQuaternionMsgFromYaw(goal_direction);
+		waypoints_.geometry_msg.header.frame_id = world_frame_;
+	}
+}
+
 void WaypointsNavigation::sleep(){
 	rate_.sleep();
 	ros::spinOnce();
@@ -44,12 +79,15 @@ void WaypointsNavigation::sleep(){
 
 void WaypointsNavigation::run(){
 	while(ros::ok()){
+
+
+
 		sleep();
 	}
 }
 
-bool readFile(const std::string &filename){
-	waypoints_.poses.clear();
+bool WaypointsNavigation::readFile(const std::string &filename){
+	waypoints_.clear();
 	try{
 			std::ifstream ifs(filename.c_str(), std::ifstream::in);
 			if(ifs.good() == false){
@@ -72,17 +110,17 @@ bool readFile(const std::string &filename){
 					const YAML::Node *wp_node = node.FindValue("waypoints");
 			#endif
 
-			geometry_msgs::Pose pose;
+			Waypoint wp;
 			if(wp_node != NULL){
 					for(int i=0; i < wp_node->size(); i++){
 
-							(*wp_node)[i]["point"]["x"] >> pose.position.x;
-							(*wp_node)[i]["point"]["y"] >> pose.position.y;
-							(*wp_node)[i]["point"]["z"] >> pose.position.z;
-							(*wp_node)[i]["point"]["floor"] >> pose.position.z;
+							(*wp_node)[i]["point"]["x"] >> wp.geometry_msg.position.x;
+							(*wp_node)[i]["point"]["y"] >> wp.geometry_msg.position.y;
+							(*wp_node)[i]["point"]["z"] >> wp.geometry_msg.position.z;
+							(*wp_node)[i]["point"]["floor"] >> wp.floor;
 
 
-							waypoints_.poses.push_back(pose);
+							waypoints_.push_back(wp);
 
 					}
 			}else{
@@ -97,17 +135,17 @@ bool readFile(const std::string &filename){
 			#endif
 
 			if(fp_node != NULL){
-					(*fp_node)["pose"]["position"]["x"] >> pose.position.x;
-					(*fp_node)["pose"]["position"]["y"] >> pose.position.y;
-					(*fp_node)["pose"]["position"]["z"] >> pose.position.z;
-					(*fp_node)["pose"]["position"]["floor"] >> pose.position.z;
+					(*fp_node)["pose"]["position"]["x"] >> wp.geometry_msg.position.x;
+					(*fp_node)["pose"]["position"]["y"] >> wp.geometry_msg.position.y;
+					(*fp_node)["pose"]["position"]["z"] >> wp.geometry_msg.position.z;
+					(*fp_node)["pose"]["position"]["floor"] >> wp.floor;
 
-					(*fp_node)["pose"]["orientation"]["x"] >> pose.orientation.x;
-					(*fp_node)["pose"]["orientation"]["y"] >> pose.orientation.y;
-					(*fp_node)["pose"]["orientation"]["z"] >> pose.orientation.z;
-					(*fp_node)["pose"]["orientation"]["w"] >> pose.orientation.w;
+					(*fp_node)["pose"]["orientation"]["x"] >> wp.geometry_msg.orientation.x;
+					(*fp_node)["pose"]["orientation"]["y"] >> wp.geometry_msg.orientation.y;
+					(*fp_node)["pose"]["orientation"]["z"] >> wp.geometry_msg.orientation.z;
+					(*fp_node)["pose"]["orientation"]["w"] >> wp.geometry_msg.orientation.w;
 
-					waypoints_.poses.push_back(pose);
+					waypoints_.push_back(wp);
 
 			}else{
 					return false;
@@ -127,12 +165,5 @@ bool readFile(const std::string &filename){
 int main(int argc, char *argv[]){
 	ros::init(argc, argv, ROS_PACKAGE_NAME);
 	WaypointsNavigation w_nav;
-
-	geometry_msgs::Point p;
-	p.x = -3;
-	w_nav.startNavigationGL(p);
-	w_nav.sleep();
-	w_nav.run();
-
 	return 0;
 }
