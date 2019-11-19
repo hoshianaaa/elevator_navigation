5746e1f94203141f05993ad4a5f3e7ad2c045f48
hoshianaaa <hoshina12345@gmail.com>
2019-11-19 11:08:12 +0900

    improve search panel program

--- elevator_waypoints_nav/src/panel_action_node.cpp
+++ elevator_waypoints_nav/src/panel_action_node.cpp
@@ -1,12 +1,13 @@
 #include <ros/ros.h>
 #include <elevator_waypoints_nav/panel_action.h>
+#include <elevator_waypoints_nav/search_panel.h>
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "panel_action_node");
+	
+	SearchPanel sp;
+	sp.run(1);
+	sp.run(0);
 
-	PanelAction action;
-	action.run();
-
-	ros::spin();
 	return (0);
 }
--- elevator_waypoints_nav/src/search_panel.cpp
+++ elevator_waypoints_nav/src/search_panel.cpp
@@ -5,9 +5,17 @@ SearchPanel::SearchPanel()
 	std::cout << "panel action class" << std::endl;
 	velocity_pub_ = n_.advertise<geometry_msgs::Twist>("icart_mini/cmd_vel", 1);
 	darknet_sub_ = n_.subscribe("darknet_data", 1, &SearchPanel::darknetCallback, this);
+	scan_sub_ = n_.subscribe("scan", 1, &SearchPanel::scanCallback, this);
+	
 	darknet_lock_ = 0;
+	darknet_data_ = 0;
 	robot_frame_  = "base_link";
 	global_frame_ = "odom";
+	freq_ = 10;
+	min_scan_ = 10000;
+
+	get_robot_pose();
+	home_point_ = robot_point_;
 }
 
 
@@ -15,27 +23,131 @@ SearchPanel::SearchPanel()
 void SearchPanel::darknetCallback(const std_msgs::Int32Ptr& msg){
 	std::cout << "darknet callback" << std::endl;
 	darknet_lock_ = 0;
+	darknet_data_ = msg->data;
 }
 
-bool SearchPanel::rotate()
-{
-while(1){
-	std::cout << "search panel rotate" << std::endl;
-}
-
-}
-
-bool SearchPanel::darknet_wait(){
-	darknet_lock_ = 1;
-	while(darknet_lock_){
-		std::cout << "darknet lock" << std::endl;
+void SearchPanel::scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
+	
+/*	
+	if(!tf_listener_.waitForTransform(
+		msg->header.frame_id,
+		"/base_link",
+		msg->header.stamp + ros::Duration().fromSec(msg->ranges.size()*msg->time_increment),
+		ros::Duration(1.0))){
+	std::cout << "scan callback transform error" << std::endl;	
+	return;
 	}
-	return 1;
+	
+	sensor_msgs::PointCloud2 cloud2;
+	
+  	projector_.transformLaserScanToPointCloud("/base_link",*msg,
+          cloud2,tf_listener_);
+	
+	pcl::PointCloud<pcl::PointXYZI> pcl_cloud;
+	pcl::fromROSMsg(cloud2, pcl_cloud);
+*/
+	min_scan_ = 10000;
+	for (int i=0;i<msg->ranges.size();i++){
+		double d = msg->ranges[i];
+		if (d > 0.3 && d < min_scan_)min_scan_ = d;
+	}
+	std::cout << "min_scan:" << min_scan_ << std::endl;
 }
 
-void SearchPanel::run()
+bool SearchPanel::rotate(double angle)
 {
+	ros::Rate loop_rate(freq_);
+	get_robot_pose();
+	double start_angle = robot_point_.z;
+	double target_angle = start_angle + angle; 
 
+	if (target_angle > M_PI)target_angle -= 2*M_PI;
+	else if (target_angle < -M_PI)target_angle += 2*M_PI;
+
+
+	while(!((robot_point_.z > target_angle - 0.1) && (robot_point_.z < target_angle + 0.1))){
+		get_robot_pose();
+		std::cout << "target angle:" << target_angle;
+		std::cout << " now :" << robot_point_.z;	
+		geometry_msgs::Twist vel;
+		if (angle > 0)vel.angular.z = 0.3;
+		else vel.angular.z = -0.3;
+		velocity_pub_.publish(vel);
+		std::cout << " ang vel :" << vel.angular.z << std::endl;	
+		loop_rate.sleep();
+		ros::spinOnce();
+}
+	
+}
+
+bool SearchPanel::darknet_wait(long wait_time){
+	darknet_lock_ = 1;
+	long begin = ros::Time::now().sec;
+	std::cout << "begin:" << begin << std::endl;
+	while(1){
+		ros::spinOnce();
+		long timer = ros::Time::now().sec - begin;
+		std::cout << "timer:" << timer << std::endl;
+		std::cout << "darknet lock" << std::endl;
+		if (!darknet_lock_){
+			std::cout << "found panel" << std::endl;
+			return 1;
+		}
+		else if(timer > wait_time){
+			std::cout << "time out" << std::endl;
+			return 0;
+		}
+	}
+}
+
+bool SearchPanel::go_panel(double stop_distance){
+	ros::Rate loop_rate(freq_);
+	while(1){
+		if (min_scan_ < stop_distance)break;
+		geometry_msgs::Twist vel;
+		vel.linear.x = 0.3;
+		velocity_pub_.publish(vel);
+		std::cout << "straight" << std::endl;
+		loop_rate.sleep();
+		ros::spinOnce();
+	}
+}
+
+bool SearchPanel::back_home(){
+	ros::Rate loop_rate(freq_);
+	double stop_distance = 0.5;
+	while(1){
+		get_robot_pose();
+		double dx = robot_point_.x - home_point_.x;
+		double dy = robot_point_.y - home_point_.y;
+		double now_dis = std::sqrt(dx * dx + dy * dy);
+		if (now_dis < stop_distance)break;
+		geometry_msgs::Twist vel;
+		vel.linear.x = -0.3;
+		velocity_pub_.publish(vel);
+		std::cout << "straight" << std::endl;
+		loop_rate.sleep();
+		ros::spinOnce();
+	}
+}
+
+
+void SearchPanel::run(bool inverse)
+{
+	int counter = 0;
+	double k = -0.003;
+	while(1){
+		if (inverse)rotate(-M_PI/4);
+		else rotate(M_PI/4);
+		if (darknet_wait(5))break;
+	}
+	std::cout << "darknet_data:" << darknet_data_ << std::endl;
+	rotate(darknet_data_ * k);
+	go_panel(0.6);	
+	back_home();
+	std::cout << "rp:" << robot_point_.z << "hp:" << home_point_.z << std::endl;
+	if (inverse)rotate(-home_point_.z + robot_point_.z);
+	else rotate(home_point_.z - robot_point_.z);
 }
 
 bool SearchPanel::get_robot_pose(){
