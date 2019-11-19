8a43a881db87008ac119463d0a4b17593e832f09
hoshianaaa <hoshina12345@gmail.com>
2019-11-19 19:20:19 +0900

    change name search_panel to panel_action

--- elevator_waypoints_nav/src/panel_action_node.cpp
+++ elevator_waypoints_nav/src/panel_action_node.cpp
@@ -1,13 +1,11 @@
 #include <ros/ros.h>
 #include <elevator_waypoints_nav/panel_action.h>
-#include <elevator_waypoints_nav/search_panel.h>
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "panel_action_node");
 	
-	SearchPanel sp;
-	sp.run(1);
-	sp.run(0);
+	PanelAction pa;
+	pa.run(1);
 
 	return (0);
 }
