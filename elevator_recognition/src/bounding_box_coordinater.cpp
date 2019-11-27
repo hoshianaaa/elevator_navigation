4f79ac98286e48aa1d0f1c5ddba124c3cbe6c376
hoshianaaa <hoshina12345@gmail.com>
2019-11-27 19:30:34 +0900

    update

--- elevator_recognition/src/bounding_box_coordinater.cpp
+++ elevator_recognition/src/bounding_box_coordinater.cpp
@@ -4,14 +4,17 @@
 #include <geometry_msgs/Point.h>
 #include <string>
 #include <limits>
+#include <boost/thread/thread.hpp>
 
 class boundingBoxCoordinater
 {
 public:
   boundingBoxCoordinater();
+  void run();
 private:
   ros::NodeHandle nh_;
   tf::TransformBroadcaster br_;
+  tf::Transform transform_;
   ros::Subscriber cloud_sub_;
   ros::Subscriber bounding_box_pos_sub_;
   void cloudCallback(const sensor_msgs::PointCloud2ConstPtr& msgs);
@@ -29,6 +32,7 @@ boundingBoxCoordinater::boundingBoxCoordinater(){
   target_frame_ = "button";
   cloud_sub_ = nh_.subscribe(cloud_topic_, 1, &boundingBoxCoordinater::cloudCallback, this);
   bounding_box_pos_sub_ = nh_.subscribe(bounding_box_pos_topic_, 1, &boundingBoxCoordinater::boundingBoxPosCallback, this);
+  int tf_broad_rate = 10;
 }
 
 void boundingBoxCoordinater::cloudCallback(const sensor_msgs::PointCloud2ConstPtr&msgs){
@@ -39,18 +43,21 @@ void boundingBoxCoordinater::cloudCallback(const sensor_msgs::PointCloud2ConstPt
 void boundingBoxCoordinater::boundingBoxPosCallback(const geometry_msgs::PointConstPtr& msgs){
   std::cout << "bounding box callback" << std::endl;  
 
+  int bx = msgs->x;
+  int by = msgs->y;
+
   float x, y, z;
-  getCloudPoint((int)msgs->x, (int)msgs->y, x, y, z);
+  while(1){
+    getCloudPoint(bx, by, x, y, z);
+    if (!isnan(x))break;
+    bx++;
+  }
   std::cout << "x:" << x << " y:" << y << " z:" << z << std::endl;
   
-  tf::Transform transform;
-  transform.setOrigin( tf::Vector3(x, y, z) );
+  transform_.setOrigin( tf::Vector3(x, y, z) );
   tf::Quaternion q;
   q.setRPY(0, 0, 0); 
-  transform.setRotation(q);
-  br_.sendTransform(tf::StampedTransform(transform, ros::Time::now(), sensor_frame_, target_frame_));
-
-
+  transform_.setRotation(q);
 }
 
 bool boundingBoxCoordinater::getCloudPoint(int x, int y, float &px, float &py, float &pz){
@@ -66,14 +73,21 @@ bool boundingBoxCoordinater::getCloudPoint(int x, int y, float &px, float &py, f
   return 1;
 }
 
+void boundingBoxCoordinater::run(){
+  ros::Rate r(10);
+  while (ros::ok())
+  {
+    br_.sendTransform(tf::StampedTransform(transform_, ros::Time::now(), sensor_frame_, target_frame_));
+    std::cout << "run" << std::endl;  
+    ros::spinOnce();
+    r.sleep();
+  }
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "bounding_box_coordinater");
   boundingBoxCoordinater bc;
-  ros::Rate r(10);
-  while(ros::ok()){
-    ros::spinOnce();
-    r.sleep();
-  }
+  bc.run();
   return 0;
 }
